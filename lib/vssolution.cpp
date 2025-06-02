0893bf56b9d4c3abc3f2be573ef807a7d99202b0
camierjs <camierjs@gmail.com>
2025-06-02 11:51:34 -0700

    filter out not supported basis types

--- lib/vssolution.cpp
+++ lib/vssolution.cpp
@@ -2111,8 +2111,11 @@ void VisualizationSceneSolution::PrepareDofNumbering()
    auto *rsol_fes = rsol->FESpace();
    const auto *rsol_fec = rsol_fes->FEColl();
    FiniteElementSpace rdof_fes(mesh, rsol_fec);
+   FiniteElementSpace flat_fes(mesh, rsol_fec->Clone(1), rsol_fes->GetVDim());
+   // filter out not supported basis types
+   const bool force_non_comforming = sol->Size() != flat_fes.GetNDofs();
 
-   if (shading == Shading::Noncomforming)
+   if (shading == Shading::Noncomforming || force_non_comforming)
    {
       Vector vals;
       for (int e = 0; e < ne; e++)
@@ -2132,103 +2135,10 @@ void VisualizationSceneSolution::PrepareDofNumbering()
    }
    else if (shading == Shading::Flat || shading == Shading::Smooth)
    {
-      FiniteElementSpace flat_fes(mesh, rsol_fec->Clone(1), rsol_fes->GetVDim());
       dbg("sol->Size():{}", sol->Size());
       dbg("flat_fes.GetNDofs():{}", flat_fes.GetNDofs());
       dbg("flat_fes.GetVSize():{}", flat_fes.GetVSize());
       dbg("vdim:{}", rsol_fes->GetVDim());
-
-      if (sol->Size() != flat_fes.GetNDofs())
-      {
-         Vector vals;
-         for (int e = 0; e < ne; e++)
-         {
-            if (!el_attr_to_show[mesh->GetAttribute(e) - 1]) { continue; }
-            const auto dx = 0.05 * GetElementLengthScale(e);
-            const IntegrationRule &ir = rsol_fes->GetFE(e)->GetNodes();
-            GetRefinedValues(e, ir, vals, tr);
-            rdof_fes.GetElementDofs(e, dofs);
-            rdof_fes.AdjustVDofs(dofs);
-            for (int q = 0; q < ir.GetNPoints(); q++)
-            {
-               const real_t x[3] = {tr(0,q), tr(1,q), vals[q]};
-               DrawNumberedMarker(d_nums_buf, x, dx, dofs[q]);
-            }
-         }
-
-         /*int btype = -1;
-
-         FiniteElementCollection *fec = nullptr;
-
-         if (auto *nf_fec = dynamic_cast<const ND_FECollection*>(rsol_fec))
-         {
-            btype = nf_fec->GetClosedBasisType();
-            dbg("ND_FECollection btype:{}", btype);
-            fec = new ND_FECollection(rsol_fec->GetOrder(), mesh->Dimension(), btype);
-         }
-         if (auto *rt_fec = dynamic_cast<const RT_FECollection*>(rsol_fec))
-         {
-            btype = rt_fec->GetClosedBasisType();
-            dbg("RT_FECollection btype:{}", btype);
-            fec = new RT_FECollection(rsol_fec->GetOrder(), mesh->Dimension(), btype);
-         }
-         if (auto *rt_fec = dynamic_cast<const H1_FECollection*>(rsol_fec))
-         {
-            btype = rt_fec->GetBasisType();
-            dbg("H1_FECollection btype:{}", btype);
-            fec = new H1_FECollection(rsol_fec->GetOrder(), mesh->Dimension(), btype);
-         }
-         if (auto *rt_fec = dynamic_cast<const L2_FECollection*>(rsol_fec))
-         {
-            btype = rt_fec->GetBasisType();
-            dbg("L2_FECollection btype:{}", btype);
-            fec = new L2_FECollection(rsol_fec->GetOrder(), mesh->Dimension(), btype);
-         }
-
-         MFEM_VERIFY(btype >= 0, "Unsupported FECollection type for LOR discretization");
-         // H1_FECollection h1_fec(rsol_fec->GetOrder() + 1, mesh->Dimension(), btype);
-         FiniteElementSpace flat_h1_fes(mesh, fec);
-         FiniteElementSpace flat_ho_fes(mesh, fec, rsol_fes->GetVDim());
-
-         const LORDiscretization lor_discretization(flat_ho_fes);
-         auto &lor_fes = lor_discretization.GetFESpace();
-         const auto &lor_perm = lor_discretization.GetDofPermutation();
-         MFEM_VERIFY(flat_ho_fes.GetNDofs() == lor_fes.GetNDofs(),
-                     "LOR space does not match the solution size");
-
-         InterpolationGridTransfer gt(flat_h1_fes, lor_fes);
-         GridFunction flat_sol(&flat_h1_fes, sol->HostReadWrite()), lor_sol(&lor_fes);
-         gt.ForwardOperator().Mult(flat_sol, lor_sol);
-
-         // store 'dx' for the sol mesh elements
-         std::map<int, double> dx;
-         for (int e = 0; e < ne; e++)
-         {
-            mesh->GetPointMatrix(e, tr);
-            ShrinkPoints(tr, e, 0, 0);
-            const auto dx_e = 0.05 * GetElementLengthScale(e);
-            flat_ho_fes.GetElementDofs(e, dofs);
-            for (int d = 0; d < dofs.Size(); d++) { dx[dofs[d]] = dx_e; }
-         }
-
-         // use the LOR mesh, fes & dofs
-         auto *lor_mesh = lor_fes.GetMesh();
-         for (int e = 0; e < lor_mesh->GetNE(); e++)
-         {
-            lor_fes.GetElementDofs(e, dofs);
-            lor_mesh->GetPointMatrix(e, tr);
-            const auto &ir = lor_fes.GetFE(e)->GetNodes();
-            for (int q = 0; q < ir.GetNPoints(); q++)
-            {
-               const int dof = dofs[lor_perm[q]];
-               const real_t x[3] = {tr(0,q), tr(1,q), LogVal(lor_sol(dof))};
-               MFEM_VERIFY(dof >= 0, "Invalid dof index");
-               DrawNumberedMarker(d_nums_buf, x, dx.at(dof), dof);
-            }
-         }*/
-         updated_bufs.emplace_back(&d_nums_buf);
-         return;
-      }
 
       MFEM_VERIFY(sol->Size() == flat_fes.GetNDofs(),
                   "Flat space does not match the solution size");
